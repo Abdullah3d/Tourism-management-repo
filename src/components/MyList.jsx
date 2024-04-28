26f11d56e297b0b9ce1ac609c8241407b28af98e
Abdullah3d <abdullah.ibrarhim.609@gmail.com>
2024-04-29 03:16:43 +0600

    added table section

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,12 +1,41 @@
+import { useLoaderData } from "react-router-dom";
 import Footer from "../pages/Footer";
+import { useState } from "react";
 
 const MyList = () => {
+    const loadedMyLists = useLoaderData();
+    const [users, setUsers] = useState(loadedMyLists);
+
+
     return (
-        <div>
-            <h2>My List</h2>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    {
+                        loadedMyLists.map(list => <tr key={list._id}>
+                            <th>1</th>
+                            <td>{list.name}</td>
+                            <td>{list.country}</td>
+                            <td>{list.description}</td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
             <Footer></Footer>
         </div>
+
     );
 };
 
 export default MyList;
+
--- src/main.jsx
+++ src/main.jsx
@@ -49,7 +49,8 @@ const router = createBrowserRouter([
       },
       {
         path: "myList",
-        element: <PrivetRoute><MyList></MyList></PrivetRoute>
+        element: <PrivetRoute><MyList></MyList></PrivetRoute>,
+        loader: () => fetch('http://localhost:5000/mylist')
       },
       {
         path: "login",
